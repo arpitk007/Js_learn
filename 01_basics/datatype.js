620dc9c0a1d5ef79803ff647b10c24b43c9ced6c
arpitk007 <111194456+arpitk007@users.noreply.github.com>
2024-05-08 19:01:46 +0000

    js learn update

---
+++ 01_basics/datatype.js
@@ -0,0 +1,22 @@
+"use strict" // all code written or follow new standrads
+
+//alert(3+3)  we are using node js note browser it happen only in browser
+
+console.log(3
+    +
+    3
+)  //not a good code readbility 
+
+let Name="Sangam" //string 
+let RollNo="1900330100049" //number 
+let bool=true  //booleen
+
+//bigint for big integers values 
+//null
+//undefined 
+
+console.log(typeof(null))  //typeof tell types of variables null is object type 
+console.log(typeof(undefined)) //undefined type
+
+
+
