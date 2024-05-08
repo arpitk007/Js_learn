4e4c797ea37299c4b07fd1d584550647c324ba67
arpitk007 <111194456+arpitk007@users.noreply.github.com>
2024-05-08 17:52:03 +0000

    jslearn update

---
+++ 01_basics/variables.js
@@ -0,0 +1,15 @@
+const accountId=22210
+let accountEmail="arpit@google.com"
+var accountPassword="1jaipur@2"
+accountCity="Ghazipur"
+let accountState
+
+// const=12345 not allowed 
+
+accountEmail="sangam@microsoft.com"
+accountPassword="@132jaipi"
+accountCity="Subhakharpur"
+
+/* prefer not to use var because of problem in block scope and functional scope */
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
