74c65b89e72d6b274716fdc66ddafd8c56c9f56d
arpitk007 <111194456+arpitk007@users.noreply.github.com>
2024-05-10 06:30:48 +0000

    Datatype summary

---
+++ 01_basics/datatypesumm.js
@@ -0,0 +1,25 @@
+//types of data type in js basic of memory 
+
+//premitive: 7 types (Value)
+
+//string, Number,boolean, null, undefined ,bigInt,Symbol -basic use for symbol
+
+//non-premitive data types(refrence)
+
+//Array ,object ,function
+
+let heros=["Ironman","Saktiman","SabkaGod Hanuman"]
+console.log(heros)
+
+let myobj={
+    Name : "Sangam",
+    age :" 22",
+}
+
+console.log(typeof(myobj))
+
+const myfunction=function(){
+    console.log("hello Sangam")
+}
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
