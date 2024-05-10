dccb292c903afffd16af301e43892045202a2fef
arpitk007 <111194456+arpitk007@users.noreply.github.com>
2024-05-10 05:52:49 +0000

    compare config

---
+++ 01_basics/compare.js
@@ -0,0 +1,25 @@
+/*
+console.log(2>1)
+console.log(2>=1)
+console.log(2<1)      simple comparison
+console.log(2<1)
+console.log(2==2)
+console.log(2!=1)  */
+
+console.log("2">1)    //en dono me ye ho raha hain ki jo string hain wo no me change ho ja rahi hain
+console.log(2>"1")
+
+console.log(2===2)   //ye true aayega kyuki 2=2 ek = ya phir ===
+console.log("2"===2)   //ye false aata hain kyuki esme string num me chanege nhi hota hain
+
+//null and undefined 
+console.log(null>0)   //yaha pe kya ho rha hain ye kabhi Nan me convert to kabhi 0 me
+console.log(null<0)
+console.log(null>=0)
+console.log(null<=0)  //esiliye esme true aa raha  
+
+console.log(undefined>0)   
+console.log(undefined<0)     //false aayega kyuki sab Nan me change hote hain
+console.log(undefined<=0)
+console.log(undefined>=0)
+
