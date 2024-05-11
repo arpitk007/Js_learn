e2f9d40ead8089355794aa42250609da8182b6fb
arpitk007 <111194456+arpitk007@users.noreply.github.com>
2024-05-11 18:11:57 +0000

    strings

---
+++ 01_basics/string.js
@@ -0,0 +1,8 @@
+let myName="arpit" //simple decelration of string
+let repoCount=10
+
+let finalString=(myName+repoCount+"Coder")  //not a goode way or old way
+
+console.log(finalString)
+
+console.log(`Hello i am ${myName} and git repo cout ${repoCount}`)  //string interpolation
