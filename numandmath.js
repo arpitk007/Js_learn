41db2ab6e4fefcb8d49c7dd300e0646daa596592
arpitk007 <111194456+arpitk007@users.noreply.github.com>
2024-05-13 16:48:37 +0000

    nums and math

--- numandmath.js
+++ numandmath.js
@@ -1,6 +1,8 @@
 const UniversalNO=100
 
 //new type to make num data type declaration
-const balance=New Number(1000)
+const balance=New Number(100)
 console.log(balance)
+
+console.log(UniversalNO)
 
