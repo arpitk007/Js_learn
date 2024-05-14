042e8045121f23caf1124e2032e2a59b86000f20
arpitk007 <111194456+arpitk007@users.noreply.github.com>
2024-05-14 17:58:59 +0000

    nums and math

---
+++ 01_basics/numandmath.js
@@ -0,0 +1,7 @@
+let RollNo=1900330100049
+const balance=new Number(100)  //new type to declare a num datatypes
+console.log(balance)
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(2)) // adding .00 after current num
+
