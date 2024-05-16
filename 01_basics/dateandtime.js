3f87df9e27d51ca268ba16338d49710fd39d1f25
arpitk007 <111194456+arpitk007@users.noreply.github.com>
2024-05-16 17:49:32 +0000

    date & time

---
+++ 01_basics/dateandtime.js
@@ -0,0 +1,8 @@
+//Dates
+let mydate=new Date()
+console.log(mydate)      //intinal formate 
+
+console.log(mydate.toString())
+console.log(mydate.toDateString())  // it will show cordinated universal time with date day month
+console.log(mydate.toLocaleString())  //it will show date in time in am/pm formate
+console.log(typeof(mydate))    //it is object type 
