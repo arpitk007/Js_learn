4a6c82a66381b06b505e09e04b89df7913657efe
arpitk007 <111194456+arpitk007@users.noreply.github.com>
2024-05-22 11:57:41 +0000

    object update

--- 02_basics/object1.js
+++ 02_basics/object1.js
@@ -1,29 +1,29 @@
 // singleton
-// Object.create
+// Object.create   constructer method ke sath object creation
 
 // object literals
 
-const mySym = Symbol("key1")
+const mySym = Symbol("key1")  //symbol declaration 
 
 
 const JsUser = {
-    name: "Hitesh",
-    "full name": "Hitesh Choudhary",
-    [mySym]: "mykey1",
+    name: "Arpit",
+    "full name": "Arpit Maurya",  //agar aise object define hota hain to 
+    [mySym]: "mykey1",    //adding symbol in keys
     age: 18,
-    location: "Jaipur",
-    email: "hitesh@google.com",
+    location: "Ghazipur",
+    email: "arpit@google.com",
     isLoggedIn: false,
     lastLoginDays: ["Monday", "Saturday"]
 }
-// console.log(JsUser.email)
-// console.log(JsUser["email"])
-// console.log(JsUser["full name"])
-// console.log(JsUser[mySym])
+console.log(JsUser.email)
+ console.log(JsUser["email"])   //aise access kar sakte hain
+ console.log(JsUser["full name"])
+ console.log(JsUser[mySym])       //symbol printing
 
-JsUser.email = "hitesh@chatgpt.com"
-// Object.freeze(JsUser)
-JsUser.email = "hitesh@microsoft.com"
+JsUser.email = "sangam@chatgpt.com"
+// Object.freeze(JsUser)   using this we can free object value that noone can change
+JsUser.email = "arpit@microsoft.com"
 // console.log(JsUser);
 
 JsUser.greeting = function(){
